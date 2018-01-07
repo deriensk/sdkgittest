0217f7c5b45b8ba6f2541fd605abf9e181afb13b
Sudhamsu Khanal <sudhamsukhanal@gmail.com>
2018-01-08 00:01:31 +0545

    sdk_branch_1 added

--- sdkgittest.py
+++ sdkgittest.py
@@ -195,3 +195,10 @@ bst.preorder()
 #bst.inorder()
 print(bst.remove(10))
 bst.preorder()
+
+
+def bubble_sort1(A):
+	for i in range (0, len(A) - 1):
+		for j in range (0, len(A) - i - 1):
+			if A[j] > A[j+1]:
+				A[j], A[j+1] = A[j+1], A
